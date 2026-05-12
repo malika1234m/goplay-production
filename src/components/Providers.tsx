7194a69a133d0310e85932ba8c8fae4fd735445d
malika1234m <malikanishnatha2@gmail.com>
2026-05-12 21:02:13 +0530

    adding toploader

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,7 +1,18 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import NextTopLoader from "nextjs-toploader";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <NextTopLoader
+        color="#22c55e"
+        height={3}
+        showSpinner={false}
+        shadow="0 0 10px #22c55e, 0 0 5px #22c55e"
+      />
+      {children}
+    </SessionProvider>
+  );
 }
